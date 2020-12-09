c9977de992a7cae0293c3f3f87866eade421cb92
hydra <hydrawork@gmail.com>
2020-12-09 16:24:45 +0800

    Make status bar and actionbar transparent

--- app/src/main/java/com/hydra/starbucksmock/MainActivity.kt
+++ app/src/main/java/com/hydra/starbucksmock/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.hydra.starbucksmock
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.View
+import android.view.WindowManager
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.jaeger.library.StatusBarUtil
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,9 +25,21 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        StatusBarUtil.setTransparent(this)
+        setToolbar()
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
+    }
+
+    private fun setToolbar() {
+        //val toolBar: Toolbar = findViewById(R.id.action_bar)
+        //toolBar.background.alpha = 0;
     }
 }
